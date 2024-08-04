b49a50f377cd61e044896648d4c2ddbebd203bf9
Richard Reis <167144386+richxrdreis@users.noreply.github.com>
2024-08-03 22:44:11 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,6 @@ function PegarNumeroAleatorioDoComputador() {
   return opcao[aleatorioIndex];
 }
 
-let pontosPlayer = 0;
-let pontosComputador = 0;
-
 function jogadorVenceRound(player, computador) {
   return (
     (player === "Pedra" && computador === "Tesoura") ||
@@ -15,8 +12,12 @@ function jogadorVenceRound(player, computador) {
   );
 }
 
+let pontosPlayer = 0;
+let pontosComputador = 0;
+
 function PegarResultadoRound(opcaoUsuario) {
   const resultadoComputador = PegarNumeroAleatorioDoComputador();
+
   if (jogadorVenceRound(opcaoUsuario, resultadoComputador)) {
     pontosPlayer++;
     return `Você venceu! ${opcaoUsuario} ganhou de ${resultadoComputador}`;
@@ -32,13 +33,34 @@ const pontosDoJogador = document.getElementById("jogador-pontuou");
 const pontosDoComputadorMsg = document.getElementById("computador-pontuou");
 const resultadoDoRoundMsg = document.getElementById("resultado-msg");
 const opcaoContainer = document.getElementById("opcao-container");
-const resetarJogo = document.getElementById("reiniciar-jogo-butao");
+const resetarJogoBtn = document.getElementById("reiniciar-jogo-butao");
 
 function mostrarResultado(opcaoUsuario) {
   resultadoDoRoundMsg.innerText = PegarResultadoRound(opcaoUsuario);
   pontosDoComputadorMsg.innerText = pontosComputador;
   pontosDoJogador.innerText = pontosPlayer;
+  if (pontosPlayer === 3 || pontosComputador === 3) {
+    resultadoDoRoundMsg.innerText = `${
+      pontosPlayer === 3 ? "Player" : "Computador"
+    } Venceu o jogo!`;
+
+    resetarJogoBtn.style.display = "block";
+    opcaoContainer.style.display = "none";
+  }
 }
+
+function reiniciarJogo() {
+  // ALTERAÇÕES POSSÍVEISS
+  pontosPlayer = 0;
+  pontosComputador = 0;
+  pontosDoJogador.innerText = pontosPlayer;
+  pontosDoComputadorMsg.innerText = pontosComputador;
+  resultadoDoRoundMsg.innerText = "";
+  resetarJogoBtn.style.display = "none";
+  opcaoContainer.style.display = "block";
+}
+
+resetarJogoBtn.addEventListener("click", reiniciarJogo);
 
 const pedraBtn = document.getElementById("pedra-btn");
 const papelBtn = document.getElementById("papel-btn");
